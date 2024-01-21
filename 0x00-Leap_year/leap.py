6777d57d19d0934304b80c86892b0adda26876b2
EkenoSamson <samsonekeno98@gmail.com>
2024-01-20 23:51:49 -0800

    update leap

--- 0x00-Leap_year/leap.py
+++ 0x00-Leap_year/leap.py
@@ -3,5 +3,11 @@
 
 
 def leap_year(year):
-    """ Returns True if the year is leap year; otherwise Returns False"""
+    """ Returns True if the year is leap year; otherwise Returns False
+
+    if ((year % 4 == 0) and (year % 100 != 0 or year % 400 == 0)):
+        return True
+    return False
+    """
+    
     return ((year % 4 == 0) and (year % 100 != 0 or year % 400 == 0))
