2dc26c1112614977f8bfe3e79bc3f5c8f0bfbb64
EkenoSamson <samsonekeno98@gmail.com>
2024-01-20 23:34:29 -0800

    add leap year python solution

---
+++ 0x00-Leap_year/leap.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module contains function leap_year"""
+
+
+def leap_year(year):
+    """ Returns True if the year is leap year; otherwise Returns False"""
+    return ((year % 4 == 0) and (year % 100 != 0 or year % 400 == 0))
